b58f4d899858266bf03419f4cecc0445f6e5693e
Dipokal HHJ <hhj@devent.kr>
2024-01-04 14:08:33 +0900

    init: clone board

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+import { Panel } from './components/Panel';
+
+
+const App: any = () => {
+    return (
+        <div>
+            <Panel>
+                <p>dasfd</p>
+            </Panel>
+
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Panel.tsx
@@ -0,0 +1,26 @@
+/** @jsxImportSource @emotion/react */
+
+import React from "react";
+import { css } from '@emotion/react'
+
+type PanelType = {
+    children?: any
+}
+
+function Panel({ children }: PanelType) {
+    return (
+        <div css={css({
+            display: "flex",
+            position: "absolute",
+            top: "0",
+            left: "0",
+            width: "30vw",
+            height: "100%",
+            backgroundColor: "#17171A"
+        })}>
+            {children}
+        </div>
+    )
+}
+
+export { Panel }
