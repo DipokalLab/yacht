71291e727d216499f73861360ffd2c1c75c24445
Dipokal HHJ <hhj@devent.kr>
2024-01-04 18:04:43 +0900

    feat: add dice

--- src/three/Scene.ts
+++ src/three/Scene.ts
@@ -1,4 +1,9 @@
 import * as THREE from 'three';
+import * as CANNON from 'cannon-es'
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 class Scene {
     scene: any
@@ -6,6 +11,8 @@ class Scene {
     renderer: any
     controls: any
     blocks: any
+    world: CANNON.World;
+    dice: { model: any; body: CANNON.Body; };
 
     constructor() {
 
@@ -47,21 +54,85 @@ class Scene {
         hemiLight.position.set( 0, 120, 0 );
         this.scene.add(hemiLight);
         
-        const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100 ),new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: true} ) );
-        mesh.rotation.x = - Math.PI / 2;
-        mesh.receiveShadow = true;
-        this.scene.add(mesh);
+        const floor = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100 ),new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: true} ) );
+        floor.rotation.x = - Math.PI / 2;
+        floor.receiveShadow = true;
+        this.scene.add(floor);
     
-    
-    
-        const helper = new THREE.CameraHelper( dirLight.shadow.camera );
-        this.scene.add( helper );
-        
+        this.controls = new OrbitControls( this.camera, this.renderer.domElement );
+
+        this.getDice()
+
+        this.dice = {
+            model: undefined,
+            body: undefined
+        }
+
+        this.world = new CANNON.World({
+            gravity: new CANNON.Vec3(0, -9.82, 0),
+        })
+
+        const floorBody = new CANNON.Body({
+            type: CANNON.Body.STATIC, 
+            shape: new CANNON.Plane(),
+        });
+
+        floorBody.position.copy(new CANNON.Vec3(floor.position.x, floor.position.y, floor.position.z));
+        floorBody.quaternion.copy(new CANNON.Quaternion(floor.quaternion.x, floor.quaternion.y, floor.quaternion.z, floor.quaternion.w));
+        this.world.addBody(floorBody);
+
+
+
+
         this.animate();
+
+    }
+
+
+    getDice() {
+        const loader = new GLTFLoader();
+
+        loader.load( '/public/dice.glb', ( gltf ) => {
+            const scale = 0.6
+            const model = gltf.scene
+            model.scale.set(scale, scale, scale)
+            model.rotation.set(
+                Math.random() * Math.PI,
+                0,
+                Math.random() * Math.PI
+            )
+
+            this.dice.body = new CANNON.Body({
+                mass: 0.3,
+                shape: new CANNON.Box(new CANNON.Vec3(0.6, 0.6, 0.6)),
+                position: new CANNON.Vec3(0, 10, 0),
+            });
+
+            this.dice.body.quaternion.copy(new CANNON.Quaternion(model.quaternion.x,model.quaternion.y,model.quaternion.z,model.quaternion.w))
+            this.dice.body.applyImpulse(
+                new CANNON.Vec3(-Math.random() * 3, Math.random() * 3, 0),
+                new CANNON.Vec3(0, 0, 0.2)
+            );
+            this.world.addBody(this.dice.body);
+
+            this.scene.add( model );
+            this.dice.model = model
+            
+
+        }, undefined, function ( error ) {
+
+            console.error( error );
+
+        } );
     }
 
     animate() {
         requestAnimationFrame( this.animate.bind(this) );
+
+        this.world.fixedStep()
+        this.dice.model.position.copy(this.dice.body.position);
+        this.dice.model.quaternion.copy(this.dice.body.quaternion);
+
 
         this.renderer.render( this.scene, this.camera );
     }
